81611712b3aae77af2f034429664f1aab54f383e
rojaahmed <rojaahmed21@gmail.com>
2026-03-01 22:36:52 +0300

    aqi4

--- services/aqi_services.py
+++ services/aqi_services.py
@@ -4,10 +4,24 @@ def compute_station_aqi(data):
     results = {}
 
     for pollutant, value in data.items():
+        if value is None:   # veri yoksa hesaplama yapma
+            results[pollutant] = None
+            continue
+
         aqi = compute_pollutant_aqi(pollutant, value)
         results[pollutant] = aqi
 
-    station_aqi = max(results.values())
+    # None olmayanları seç
+    valid_aqi = [v for v in results.values() if v is not None]
+
+    if not valid_aqi:
+        return {
+            "pollutants": results,
+            "station_aqi": None,
+            "category": "Veri Yok"
+        }
+
+    station_aqi = max(valid_aqi)
 
     return {
         "pollutants": results,
