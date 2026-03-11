055b98b2c041b36fef6c86e623d73cb9370ca94c
rojaahmed <rojaahmed21@gmail.com>
2026-03-11 16:10:11 +0300

    son5

--- services/notification_worker.py
+++ services/notification_worker.py
@@ -1,8 +1,8 @@
-from risk_engine import disease_sensitive, is_risky
 from database import get_db
 from services.forecast_service import get_7_hour_forecast
 from services.location_service import find_nearest_station
 from services.notification_service import send_notification
+from health_engine import check_user_risk
 
 
 def run_notification_job():
@@ -21,8 +21,16 @@ def run_notification_job():
 
     for user in users:
 
-        lat = user["latitude"]
-        lon = user["longitude"]
+        user_data = {
+            "id": user[0],
+            "kronik_hastalik": user[1],
+            "firebase_token": user[2],
+            "latitude": user[3],
+            "longitude": user[4]
+        }
+
+        lat = user_data["latitude"]
+        lon = user_data["longitude"]
 
         station = find_nearest_station(lat, lon)
 
@@ -30,14 +38,16 @@ def run_notification_job():
 
         for p in predictions:
 
-            message = check_user_risk(user, p)
+            message = check_user_risk(user_data, p)
 
             if message:
 
                 send_notification(
-                    user["firebase_token"],
+                    user_data["firebase_token"],
                     "Hava Kalitesi Uyarısı",
                     message
                 )
 
-                print(f"Notification sent to {user['id']}")
+                print(f"Notification sent to {user_data['id']}")
+
+                break
