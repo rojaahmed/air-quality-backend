521e0db9c0c2dce44761bf86d100bf87dad3e41a
rojaahmed <rojaahmed21@gmail.com>
2026-05-01 11:10:21 +0300

    shap son

--- main.py
+++ main.py
@@ -352,23 +352,9 @@ import os
 @app.get("/shap-data")
 def get_shap_data():
 
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    file_path = "data/gunluk_tahmin_catboost.json"
 
-    file_path = os.path.join(
-        BASE_DIR,
-        "..",
-        "data",
-        "gunluk_tahmin_catboost.json"
-    )
-
-    print("PATH:", file_path)
     print("EXISTS:", os.path.exists(file_path))
-
-    if not os.path.exists(file_path):
-        return {
-            "error": "Dosya bulunamadı",
-            "path": file_path
-        }
 
     with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
